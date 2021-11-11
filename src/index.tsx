7550a481c6161237df65642792e6c04e0209edcb
joy_li <woaiyinle520@163.com>
2021-11-11 11:10:08 +0800

    react-native初始化，设置绝对路径

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { fn } from '@/utils/index';
+
+const App = () => {
+  fn();
+  return (
+    <View>
+      <Text>app</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export const fn = () => console.log('666');
